767b5fc5efe95fad41916c6180c13938d52215d1
infinityPV <31202524+infinityPV@users.noreply.github.com>
2017-11-02 12:10:55 +0100

    Update and rename InfinityPV_MCP342X.cpp to InfinityPV_MCP3426.cpp

--- InfinityPV_MCP3426.cpp
+++ InfinityPV_MCP3426.cpp
@@ -1,6 +1,6 @@
 /**************************************************************************/
 /*!
-  @file     InfinityPV_MCP342X.cpp
+  @file     InfinityPV_MCP3426.cpp
   @author   rava (infinityPV ApS)
   @license  BSD (see license.txt)
 
@@ -31,7 +31,7 @@
 
 #include <Wire.h>
 
-#include "InfinityPV_MCP342X.h"
+#include "InfinityPV_MCP3426.h"
 
 /*	static float	stepSizeTbl[] = {
 		0.001,		// 12-bit, 1X Gain
